99ab092f9cb069a107a787f5ee23fcff07e49bb1
MySharonnie <sharonneblevins@gmail.com>
2024-11-20 17:39:08 -0500

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
 
-        for (int i = 1; i <= 15; i++) {
+        for (int i = 1; i <= 12; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("Hello and welcome!");
